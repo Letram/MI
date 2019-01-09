ad4da61e940859de92d84c6010bb1f852040406d
Carlos Martel Lamas <carlos.martel105@alu.ulpgc.es>
2019-01-09 11:32:59 +0000

    Question three "done"?

--- ProyecoMI/Question_Three.m
+++ ProyecoMI/Question_Three.m
@@ -6,7 +6,7 @@ function [] = Question_Three()
     %ds = datastore('airlinesmall.csv', 'TreatAsMissing', 'NA');
     ds.SelectedVariableNames = {'DayOfWeek', 'Month', 'Cancelled', 'DepDelay'};
 
-    outds = mapreduce(ds, @holidayFlightsByDayMapper, @holidayFlightsByDayReducer);
+    outds = mapreduce(ds, @holidayFlightsByDayMapper, @holidayFlightsReducer);
     res = readall(outds);
     res = sortrows(res,1,'ascend');
     struct = table2struct(res);
@@ -23,6 +23,8 @@ function [] = Question_Three()
     flights = transpose(flights);
     cancelations = transpose(cancelations);
     combine = [flights cancelations];
+    
+    figure(1);
     subplot(2,1,1);
     bar(days, combine);
     title('Flights and cancelations by day of the week');
@@ -32,6 +34,36 @@ function [] = Question_Three()
     bar(days, delays);
     title('Average departure delay by day of the week');
     xlabel('Day of the week');
+    ylabel('Average departure delay');
+    
+    outds = mapreduce(ds, @holidayFlightsByMonthMapper, @holidayFlightsReducer);
+    res = readall(outds);
+    res = sortrows(res,1,'ascend');
+    struct = table2struct(res);
+    days = [];
+    flights = [];
+    delays = [];
+    cancelations = [];
+    for i = 1:numel(struct)
+        days = [days struct(i).Key];
+        flights = [flights struct(i).Value(1)];
+        delays = [delays struct(i).Value(2)];
+        cancelations = [cancelations struct(i).Value(3)];
+    end
+    flights = transpose(flights);
+    cancelations = transpose(cancelations);
+    combine = [flights cancelations];
+    
+    figure(2);
+    subplot(2,1,1);
+    bar(days, combine);
+    title('Flights and cancelations by month');
+    xlabel('Month');
+    ylabel('Number of flights and cancelations');
+    subplot(2,1,2);
+    bar(days, delays);
+    title('Average departure delay by month');
+    xlabel('Month');
     ylabel('Average departure delay');
 end
 
@@ -60,11 +92,36 @@ function holidayFlightsByDayMapper(data, ~, intermKVStore)
     
 end
 
+function holidayFlightsByMonthMapper(data, ~, intermKVStore)
+
+    delays = data.DepDelay; 
+    months = data.Month; 
+    
+    notNaN =~isnan(delays);
+    
+    months = months(notNaN); 
+    delays = delays(notNaN);  
+    
+    %in some files cancelations are nt counting because all data is missing
+    cancelations = data.Cancelled;
+    unparsedMonth = data.Month;
+    
+    [intermKeysMonthUnparsed,~,idxMonthUnparsed] = unique(unparsedMonth, 'stable');
+    [intermKeysMonth,~,idxMonth] = unique(months, 'stable');
+    intermValsDays = accumarray(idxMonth,delays,size(intermKeysMonth),@countsum);
+    intermValsCancelations = accumarray(idxMonthUnparsed, cancelations, size(intermKeysMonthUnparsed));
+    aux = cell2mat(intermValsDays);
+    aux = [aux intermValsCancelations];
+    out = num2cell(aux,2);
+    addmulti(intermKVStore,intermKeysMonth,out); 
+    
+end
+
 function out = countsum(x)
     out = {[length(x) sum(x)]};
 end
 
-function holidayFlightsByDayReducer(intermKey, intermValIter, outKVStore)
+function holidayFlightsReducer(intermKey, intermValIter, outKVStore)
     sum_occurences = 0;
     s = 0;
     canc = 0;
